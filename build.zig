acb89307d8e5a0c58a5de412ecabd1a7a294fc06
nat3 <nat3Github>
2025-08-09 12:09:07 +0200

    update build

--- build.zig
+++ build.zig
@@ -156,7 +156,7 @@ pub fn build(b: *std.Build) void {
     run_cmd1.step.dependOn(b.getInstallStep());
     step_run.dependOn(&run_cmd1.step);
 
-    std.debug.print("abi: {} cpu: {} os: {}", .{ target.result.abi, target.result.cpu, target.result.os });
+    // std.debug.print("abi: {} cpu: {} os: {}", .{ target.result.abi, target.result.cpu, target.result.os });
 
     const release_build = b.addExecutable(.{
         .name = "Weather-App-Local",
